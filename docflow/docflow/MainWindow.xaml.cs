979a3cfa1a49aea3d76d8f2bdcd91daf6f62b935
Kenan Abadzic <kenan.abadzic@tacta.io>
2025-04-04 22:48:34 +0200

    [2][Kenan] feat: add file picking

--- docflow/docflow/MainWindow.xaml.cs
+++ docflow/docflow/MainWindow.xaml.cs
@@ -1,36 +1,63 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System;
+using Windows.Storage.Pickers;
 
 namespace docflow
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private string _fileType = string.Empty; // Default file type
         public MainWindow()
         {
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void PickAFileButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var senderButton = sender as Button;
+            senderButton.IsEnabled = false;
+
+            PickAFileOutputTextBlock.Text = "";
+
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            var window = App.MainWindow;
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add(_fileType); // Add other file types as needed
+            openPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+
+            var file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                PickAFileOutputTextBlock.Text = "Picked file: " + file.Name;
+                //TODO: implement logic for file processing
+            }
+            else
+            {
+                PickAFileOutputTextBlock.Text = "Operation cancelled.";
+            }
+
+            senderButton.IsEnabled = true;
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            var menuItem = sender as MenuFlyoutItem;
+            if (menuItem != null)
+            {
+                DropDownButton.Content = "Pick a file type";
+                _fileType = menuItem.Text;
+                DropDownButton.Content = menuItem.Text;
+                PickAFileButton.IsEnabled = true;
+            }
+
         }
     }
 }
