73b33f9c3e3c454465837e041688c04546fa039b
acorovic1 <acorovic1@etf.unsa.ba>
2025-04-06 12:10:27 +0200

    added: user login and document selection

--- docflow/docflow/HomePage.xaml.cs
+++ docflow/docflow/HomePage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +12,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,27 +19,20 @@ using Windows.Foundation.Collections;
 namespace docflow
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class HomePage : Page
     {
-        public Frame ContentFrame => contentFrame;
-        public MainWindow()
+        public HomePage()
         {
             this.InitializeComponent();
-            contentFrame.Navigate(typeof(LoginPage));
-
-           
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void LogOut(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
-        }
+            ((App)Application.Current).m_window?.ContentFrame.Navigate(typeof(LoginPage));
 
-        private void goToLoginPage(object sender, RoutedEventArgs e)
-        {
-            LoginPage.Content = "Whoops!";
+            
         }
     }
 }
