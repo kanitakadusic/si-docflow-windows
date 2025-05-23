9811a734a7a2bbf1b9d878ebc5c279937196684f
Denan Poturak <dpoturak1@etf.unsa.ba>
2025-05-23 16:46:03 +0200

    Recognize camera device

--- docflow/docflow/DeviceSettings.xaml.cs
+++ docflow/docflow/DeviceSettings.xaml.cs
@@ -10,23 +10,74 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+
 
 namespace docflow
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DeviceSettings : Window
     {
+        private bool _initialized = false;
         public DeviceSettings()
         {
             this.InitializeComponent();
+            this.Activated += DeviceSettings_Activated;
+        }
+
+        private async void DeviceSettings_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            if (!_initialized)
+            {
+                _initialized = true;
+
+                FindCameraDeviceAsync();
+            }
+        }
+        private async void FindCameraDeviceAsync()
+        {
+            var allVideoDevicesInfo = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+
+            List<string> cameraNames = new List<string>();
+
+            if (allVideoDevicesInfo.Any())
+            {
+                foreach (var device in allVideoDevicesInfo)
+                {
+                    cameraNames.Add(device.Name); 
+                }
+            }
+            else
+            {
+
+            }
+
+            if (cameraNames.Any())
+            {
+                DevicesComboBox.ItemsSource = cameraNames;
+                DevicesComboBox.SelectedIndex = 0; 
+            }
+            else
+            {
+                DevicesComboBox.ItemsSource = new List<string> { "Nijedan uredaj nije dostupan." };
+                DevicesComboBox.SelectedIndex = 0;
+            }
+
+        }
+
+        private void OnSaveClick(object sender, RoutedEventArgs e)
+        {
+            FindCameraDeviceAsync();
+        }
+
+        private void OnRefreshClick(object sender, RoutedEventArgs e)
+        {
+            FindCameraDeviceAsync();
         }
     }
 }
